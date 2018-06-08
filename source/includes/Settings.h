a0ca4a2956225e6ff6395c108e4363dd88526df4
alexfdv <alexfdv@gmail.com>
2018-06-08 21:09:03 +0300

    Added missed file settings

---
+++ source/includes/Settings.h
@@ -0,0 +1,11 @@
+#ifndef SOURCE_INCLUDES_SETTINGS_H_
+#define SOURCE_INCLUDES_SETTINGS_H_
+
+typedef struct _Settings
+{
+    uint16 compressor_preasure_max;
+    uint16 compressor_preasure_min;
+    uint16 dummy[6];
+} Settings;
+
+#endif /* SOURCE_INCLUDES_SETTINGS_H_ */
